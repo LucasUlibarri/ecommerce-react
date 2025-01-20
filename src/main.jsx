a95ea154b7d9b44fcc884e50a10b0aa7df5a4136
Lucas Ulibarri <lucasulibarri26@gmail.com>
2025-01-20 20:45:57 -0300

    [M] Se agrego firebase al proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { initializeApp } from "firebase/app";
 import App from './App.jsx'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbwUmxV3WkC3NHAXPyRA_6gnAoxAnlkv0",
+  authDomain: "ecommerce-react-6b6a1.firebaseapp.com",
+  projectId: "ecommerce-react-6b6a1",
+  storageBucket: "ecommerce-react-6b6a1.firebasestorage.app",
+  messagingSenderId: "298682218497",
+  appId: "1:298682218497:web:8ad9c6119c2981d02a2800"
+};
+
+initializeApp(firebaseConfig);
 
 createRoot(document.getElementById('root')).render(
   <>
